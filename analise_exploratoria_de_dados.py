2d74b737e33a7f2319f03a63a570aef9683c610a
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2025-12-14 17:47:03 -0300

    refactor: make dataset autoinitialize itself

--- analise_exploratoria_de_dados.py
+++ analise_exploratoria_de_dados.py
@@ -1,4 +1,4 @@
-# %%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -41,7 +41,7 @@ class EDA:
             arquivo_faulty: Nome do arquivo com dados com falha
         """
         # Carrega os dados
-        self.df_normal, self.df_faulty = self.dataset.carregar_dados(
+        self.df_normal, self.df_faulty = self.dataset._carregar_dados(
             arquivo_normal, arquivo_faulty
         )
         
@@ -681,7 +681,7 @@ class EDA:
         plt.show()
     
     def executar_pipeline_completo(self, mostrar_diagnostico=True, mostrar_analise_univariada=True,
-                                   mostrar_sensores=False, aplicar_filtro=False):
+                                   mostrar_sensores=False, mostrar_savgol=False, aplicar_filtro=False):
         """
         Executa o pipeline completo de pré-processamento e análise
         
@@ -760,7 +760,7 @@ class EDA:
             print("ETAPA 6: APLICAÇÃO DE FILTRO SAVITZKY-GOLAY")
             print("="*60)
             df_filtered = self.aplicar_filtro_savgol(self.df_normal)
-            self.plot_raw_vs_smooth(df_filtered)
+            if mostrar_savgol: self.plot_raw_vs_smooth(df_filtered)
             resultados['df_filtered'] = df_filtered
         
         print("\n" + "="*60)
@@ -801,4 +801,3 @@ class EDA:
     # df_filtered = eda.aplicar_filtro_savgol(eda.df_normal)
     # eda.plot_raw_vs_smooth(df_filtered)
 
-#%%
